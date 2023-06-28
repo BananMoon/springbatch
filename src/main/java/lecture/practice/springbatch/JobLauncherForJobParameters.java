8c468cf5d558d44715c60ae799de8e52d5067781
BananMoon <younji1115@gmail.com>
2023-06-29 05:38:20 +0900

    style: JobParameters 관련 주석 수정

--- src/main/java/lecture/practice/springbatch/JobLauncherForJobParameters.java
+++ src/main/java/lecture/practice/springbatch/JobLauncherForJobParameters.java
@@ -12,11 +12,17 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 
 /**
- * ApplicationRunner : Bean 세팅 등 진행 후 SpringApplication.run() 수행하며 호출됨.
- * JobParameter 테스트를 위해 JobLauncher 직접 구현한다.
- * 생성한 JobParameters는 job(step) 수행 단계에서 참조될 수 있다.
+ * ApplicationRunner : Bean 세팅 등 진행 후 SpringApplication.run() 수행하며 호출됨.<br>
+ * JobParameter 테스트를 위해 JobLauncher 직접 구현한다.<br>
+ * 생성한 JobParameters는 job(step) 수행 단계에서 참조될 수 있다.<br>
+ * - JobParameters 생성하는 방법<br>
+ * 1. 소스코드 상에서 Job을 실행(run)시키기 전에 JobParameters 생성<br>
+ * 2. jar 파일 이용해 실행 요청을 날릴 때 Program Arguments로 전달하여 JobParameters 생성.<br>
+ * Ex) java -jar {~~-SNAPSHOT.jar} name=user1 seq(long)=2L tall(double)=159.9 birth(date)=2023/06/28 <br>
+ * 이때 1. spring.batch.job.enabled=true 설정<br>
+ * 2. 해당 컴포넌트 애노테이션 제거하여 자동 스프링부트에 의한 Runner가 수행되도록 한다.
   */
-//@Component    // 2. jar 파일 수행 시 Program Arguments로 전달하여 JobParameters 생성할 수 있다. 테스트 시 해당 클래스는 수행하지 않도록 한다.+ spring.batch.job.enabled=false 주석!
+@Component
 @RequiredArgsConstructor
 public class JobLauncherForJobParameters implements ApplicationRunner {
     private final JobLauncher jobLauncher;
